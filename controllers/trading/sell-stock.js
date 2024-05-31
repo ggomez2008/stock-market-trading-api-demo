247eafd221ca3cd953cd57806d6a1edf31389bf2
Gustavo Gomez <ggomez2008@gmail.com>
2024-05-31 11:47:44 -0700

    update sell stock route handler to check if user exists

--- controllers/trading/sell-stock.js
+++ controllers/trading/sell-stock.js
@@ -8,43 +8,55 @@ const sellStock = async (req, res) => {
         const mappedQuote = mapGlobalQuote(globalQuote)
         const { price } = mappedQuote
         // these queries should be in a transaction
-        const getStockBalanceQuery = `
+        const getUserQuery = `
             SELECT
-                balance
+                *
             FROM
-                portfolio
+                users
             WHERE
-                user_id=${userId}
-                AND symbol='${symbol}'
+                id=${userId}
         `
-        const getBalanceResult = await db.raw(getStockBalanceQuery)
-        const [row] = getBalanceResult.rows
-        const balance = row && row.balance
-        const hasEnoughBalance = balance >= quantity
-        if (hasEnoughBalance) {
-            const insertTradeQuery = `
-                INSERT INTO
-                    trades (user_id, transaction_type, symbol, quantity, price)
-                VALUES
-                    (${userId}, 'sell', '${symbol}', ${quantity}, ${price})
+        const userResult = await db.raw(getUserQuery)
+        const [userRow] = userResult.rows
+        if (userRow) {
+            const getStockBalanceQuery = `
+                SELECT
+                    balance
+                FROM
+                    portfolio
+                WHERE
+                    user_id=${userId}
+                    AND symbol='${symbol}'
             `
-            const insertTradeResult = await db.raw(insertTradeQuery)
-            if (insertTradeResult.rowCount > 0) {
-                const newBalance = balance - parseInt(quantity)
-                const updateBalanceQuery = `
-                    UPDATE
-                        portfolio
-                    SET
-                        balance = ${newBalance}
-                    WHERE
-                        user_id=${userId}
-                        AND symbol='${symbol}'                        
+            const getBalanceResult = await db.raw(getStockBalanceQuery)
+            const [row] = getBalanceResult.rows
+            const balance = row && row.balance
+            const hasEnoughBalance = balance >= quantity
+            if (hasEnoughBalance) {
+                const insertTradeQuery = `
+                    INSERT INTO
+                        trades (user_id, transaction_type, symbol, quantity, price)
+                    VALUES
+                        (${userId}, 'sell', '${symbol}', ${quantity}, ${price})
                 `
-                const updateResult = await db.raw(updateBalanceQuery)
-                if (updateResult.rowCount > 0) res.sendStatus(200)
-                else res.sendStatus(422)
-            }
-        } else res.sendStatus(400)
+                const insertTradeResult = await db.raw(insertTradeQuery)
+                if (insertTradeResult.rowCount > 0) {
+                    const newBalance = balance - parseInt(quantity)
+                    const updateBalanceQuery = `
+                        UPDATE
+                            portfolio
+                        SET
+                            balance = ${newBalance}
+                        WHERE
+                            user_id=${userId}
+                            AND symbol='${symbol}'                        
+                    `
+                    const updateResult = await db.raw(updateBalanceQuery)
+                    if (updateResult.rowCount > 0) res.sendStatus(200)
+                    else res.sendStatus(422)
+                }
+            } else res.sendStatus(400)
+        } else res.sendStatus(404)
     } catch (err) {
         console.error(err)
         res.sendStatus(500)
