38beccb9f214a0044d602ec0fae36e62ab8047bd
Gustavo Gomez <ggomez2008@gmail.com>
2024-05-30 23:07:54 -0700

    add buy stock route handler

---
+++ controllers/trading/buy-stock.js
@@ -0,0 +1,63 @@
+const db = require('../../db/get-db-connection')
+const { mapGlobalQuote } = require('../../utils/')
+
+const buyStock = async (req, res) => {
+    try {
+        const { userId, symbol, quantity } = req.body
+        const { globalQuote } = res.locals
+        const mappedQuote = mapGlobalQuote(globalQuote)
+        const { price } = mappedQuote
+        // these queries should be in a transaction
+        const insertTradeQuery = `
+            INSERT INTO
+                trades (user_id, transaction_type, symbol, quantity, price)
+            VALUES
+                (${userId}, 'buy', '${symbol}', ${quantity}, ${price})
+        `
+        const insertTradeResult = await db.raw(insertTradeQuery)
+        if (insertTradeResult.rowCount > 0) {
+            const getStockBalanceQuery = `
+                SELECT
+                    balance
+                FROM
+                    portfolio
+                WHERE
+                    user_id=${userId}
+                    AND symbol='${symbol}'
+            `
+            const getBalanceResult = await db.raw(getStockBalanceQuery)
+            const [row] = getBalanceResult.rows
+            if (row) {
+                const { balance } = row
+                const newBalance = balance + parseInt(quantity)
+                const updateBalanceQuery = `
+                    UPDATE
+                        portfolio
+                    SET
+                        balance = ${newBalance}
+                    WHERE
+                        user_id=${userId}
+                        AND symbol='${symbol}'                        
+                `
+                const updateResult = await db.raw(updateBalanceQuery)
+                if (updateResult.rowCount > 0) res.sendStatus(200)
+                else res.sendStatus(422)
+            } else {
+                const addToPortfolioQuery = `
+                    INSERT INTO
+                        portfolio (user_id, symbol, balance)
+                    VALUES
+                        (${userId}, '${symbol}', ${quantity})
+                `
+                const insertResult = await db.raw(addToPortfolioQuery)
+                if (insertResult.rowCount > 0) res.sendStatus(200)
+                else res.sendStatus(422)
+            }
+        } else res.sendStatus(422)
+    } catch (err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
+}
+
+module.exports = buyStock
