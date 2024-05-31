0c86ae41c6f0e521233048c2f70b1206edddc194
Gustavo Gomez <ggomez2008@gmail.com>
2024-05-31 10:51:08 -0700

    update index to add portfolio router to api path

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express()
 const port = process.env.SERVER_PORT || 3000
 const { stockRouter } = require('./routers/stocks/')
 const { tradingRouter } = require('./routers/trading/')
+const { portfolioRouter } = require('./routers/portfolio/')
 
 app.use(express.json())
-app.use('/api', stockRouter(), tradingRouter())
+app.use('/api', stockRouter(), tradingRouter(), portfolioRouter())
 app.get('/health-check', (req, res) => {
     res.sendStatus(200)
 })
