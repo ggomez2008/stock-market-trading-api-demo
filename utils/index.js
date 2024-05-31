6f5f1746ea04a40f704965168e9dcdd11c8c36f1
Gustavo Gomez <ggomez2008@gmail.com>
2024-05-30 23:12:34 -0700

    add utils to index

---
+++ utils/index.js
@@ -0,0 +1,7 @@
+const getGlobalQuoteUrl = require('./get-global-quote-url')
+const mapGlobalQuote = require('./map-global-quote')
+
+module.exports = {
+    getGlobalQuoteUrl,
+    mapGlobalQuote,
+}
