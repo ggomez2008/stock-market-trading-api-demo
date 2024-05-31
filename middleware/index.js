4160b262530b37a83cbb2b882ee5785063e55962
Gustavo Gomez <ggomez2008@gmail.com>
2024-05-30 23:09:07 -0700

    add middleware index

---
+++ middleware/index.js
@@ -0,0 +1,7 @@
+const getGlobalQuote = require('./get-global-quote')
+const validateSymbolParam = require('./validate-symbol-param')
+
+module.exports = {
+    getGlobalQuote,
+    validateSymbolParam,
+}
