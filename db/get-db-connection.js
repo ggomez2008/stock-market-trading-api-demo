3a127fe71e4e4b776f295a45772d7fe3687c52ce
Gustavo Gomez <ggomez2008@gmail.com>
2024-05-30 23:08:30 -0700

    add db connection util

---
+++ db/get-db-connection.js
@@ -0,0 +1,11 @@
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE_NAME
+    }
+})
+
+module.exports = db
